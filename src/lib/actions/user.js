8b8d93a650ba3937d053cf182f21874db77258a9
Magdalena <sheroyanmagdalena@gmail.com>
2025-11-14 04:43:24 +0400

    updated src/lib/actions/user.js

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -2,9 +2,6 @@ import User from '../models/user.model';
 
 import { connect } from '../mongodb/mongoose';
 
-import User from "../models/user.model";
-import { connect } from "../mongodb/mongoose";
-
 export const createOrUpdateUser = async (
   id,
   first_name,
